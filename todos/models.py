7578e9f55e6bf7be6091227c96836fd287d4df7a
ViniDaniel <viniciusdanielgonsalvess@gmail.com>
2024-09-29 19:34:52 -0300

    abra cadabra2

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Todo(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField(max_length=1000)
+    due_date = models.DateField()
+    priority = models.IntegerField(choices=((1, 'Low'), (2, 'Medium'), (3, 'High')))
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 # Create your models here.
